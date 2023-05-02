ecd16c41ca1adf2703944fb259dff6994bd4bf92
Dyberg <mathias.rerup-dyberg@hotmail.com>
2023-05-02 12:55:21 +0200

    Update input handler to find parentesis pairs

--- tourguide/src/Infrastructure/InputHandler.js
+++ tourguide/src/Infrastructure/InputHandler.js
@@ -9,9 +9,33 @@ class InputHandler {
         }
         return termString;
     }
+
+    findPairs(input) {
+        const len = input.split('(').length;
+        const stringArray = new Array(len).fill("");
+      
+        let currentNestedLevel = -1;
+        for (let i = 0; i < input.length; i++) {
+          const element = input.charAt(i);
+          switch (element) {
+            case '(':
+              currentNestedLevel++;
+              break;
+            case ')':
+              if (currentNestedLevel !== 0) {
+                currentNestedLevel--;
+              }
+              break;
+            default:
+              stringArray[currentNestedLevel] += element;
+              break;
+          }
+        }
+        return stringArray;
+      }
 }
 
-const t1 = "(λx.(yx))"
+const t1 = "(λx.(yx)(zp))"
 const inputHandler = new InputHandler();
-const lambdaTerm = inputHandler.buildTermFromString(t1);
+const lambdaTerm = inputHandler.findPairs(t1);
 console.log(`LambdaTerm: ${lambdaTerm}`);
