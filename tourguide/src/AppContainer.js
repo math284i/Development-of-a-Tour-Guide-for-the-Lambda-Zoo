edab483f5031a708837a78fd24d6b2c4d7366098
Anders Poulsen <s204423@student.dtu.dk>
2023-05-12 13:34:59 +0200

    something

--- tourguide/src/AppContainer.js
+++ tourguide/src/AppContainer.js
@@ -8,15 +8,25 @@ import { TreeNode } from "./Infrastructure/ImprovedDataStructur";
 
 export class AppContainer extends React.Component {
     constructor(props) {
+        this.path = [];
         super(props);
         this.handleClick = this.handleClick.bind(this);
     }
 
+
     callByName(term) {
+        //One beta step with cbn:
+        //If my left child is an abstraction (Has lambda)
+        //Then take the left childs left child and save
+        //Take the right child of my left child and substitute with my right child
+        //I become the result of the previous substitution
+        //Save me (toString) as a step in the path array
+        //Recursively call this function until no more terms are present or no more steps can be done
         return term;
     }
 
     calculate(setting) {
+        this.path.push(this.props.input)
         switch(setting) {
             case "CBN":
                 return this.callByName(this.props.input);
