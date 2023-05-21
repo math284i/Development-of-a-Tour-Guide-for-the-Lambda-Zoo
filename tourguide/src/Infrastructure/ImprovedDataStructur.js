b16f3d872cf8e1166d7585c9565de1bfa5ba725d
Dyberg <mathias.rerup-dyberg@hotmail.com>
2023-05-21 13:25:31 +0200

    Update datastructur to have reference to parent

--- tourguide/src/Infrastructure/ImprovedDataStructur.js
+++ tourguide/src/Infrastructure/ImprovedDataStructur.js
@@ -1,67 +1,62 @@
-
 export class TreeNode {
-    constructor(value) {
-        this.value = value;
-        this.leftChild = null;
-        this.rightChild = null;
+    constructor(value, parent) {
+        this.Value = value;
+        this.Parent = parent;
+        this.LeftChild = null;
+        this.RightChild = null;
     }
 
-    static findNode(root, value) {
-        if (!root) {
+    static FindNode(root, value) {
+        if (root == null) {
             return null;
         }
 
-        if (root.value === value) {
+        if (root.Value === value) {
             return root;
         }
 
-        let node = TreeNode.findNode(root.leftChild, value);
-        if (node) {
+        let node = TreeNode.FindNode(root.LeftChild, value);
+        if (node != null) {
             return node;
         }
 
-        let node2 = TreeNode.findNode(root.rightChild, value);
-        return node2 ?? null;
+        let node2 = TreeNode.FindNode(root.RightChild, value);
+        return node2 || null;
     }
 
-    static toString(root, depth = 0) {
-        if (!root) {
+    static ToString(root, depth = 0) {
+        if (root == null) {
             return "";
         }
 
-        let result = " ".repeat(depth * 2) + root.value + "\n";
-        result += TreeNode.toString(root.leftChild, depth + 1);
-        result += TreeNode.toString(root.rightChild, depth + 1);
+        let result = " ".repeat(depth * 2) + root.Value + "\n";
+        result += TreeNode.ToString(root.LeftChild, depth + 1);
+        result += TreeNode.ToString(root.RightChild, depth + 1);
         return result;
     }
 }
 
-let root = new TreeNode("APP");
-let left = new TreeNode("ABS");
-let leftleft = new TreeNode("x");
-let leftRight = new TreeNode("ABS");
-let leftRightLeft = new TreeNode("y");
-let leftRightRight = new TreeNode("APP");
-let leftRightRightLeft = new TreeNode("x");
-let leftRightRightRight = new TreeNode("z");
-
-leftRightRight.leftChild = leftRightRightLeft;
-leftRightRight.rightChild = leftRightRightRight;
+let root = new TreeNode("ABS");
+let leftChild = new TreeNode("App");
+let leftleft = new TreeNode("\\x");
+let leftRight = new TreeNode("App");
+let leftRightLeft = new TreeNode("\\y");
+let leftRightRight = new TreeNode("y");
 
 leftRight.leftChild = leftRightLeft;
 leftRight.rightChild = leftRightRight;
 
-left.leftChild = leftleft;
-left.rightChild = leftRight;
+leftChild.leftChild = leftleft;
+leftChild.rightChild = leftRight;
 
-let right = new TreeNode("ABS");
-let rightLeft = new TreeNode("z");
+let rightChild = new TreeNode("App");
+let rightLeft = new TreeNode("\\z");
 let rightRight = new TreeNode("z");
-right.leftChild = rightLeft;
-right.rightChild = rightRight;
+rightChild.leftChild = rightLeft;
+rightChild.rightChild = rightRight;
 
-root.leftChild = left;
-root.rightChild = right;
+root.leftChild = leftChild;
+root.rightChild = rightChild;
 
 console.log("ImprovedDataStructur");
 console.log(TreeNode.toString(root));
