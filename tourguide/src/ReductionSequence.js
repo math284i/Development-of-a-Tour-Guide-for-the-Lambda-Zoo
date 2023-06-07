3e0cb173cdbb5776866dd7bd3e2087039cc8d82b
Dyberg <mathias.rerup-dyberg@hotmail.com>
2023-06-07 17:11:04 +0200

    Css update

--- tourguide/src/ReductionSequence.js
+++ tourguide/src/ReductionSequence.js
@@ -13,8 +13,8 @@ export class ReductionSequence extends React.Component {
             <h2 className="title is-2">Reduction Sequence</h2>
             {this.props.path.map((step, index) => (
               <div key={index}>
-                {(index === 0) ? <h3>Initial term</h3> : <h3>Step {index}</h3>}
-                {step}
+                {(index === 0) ? <h3 className="title is-4">Initial term</h3> : <h3 className="title is-4">Step {index}</h3>}
+                <h4 className="title is-5"> {step} </h4>
               </div>
             ))}
           </div>
