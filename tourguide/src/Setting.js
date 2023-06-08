828561ea60650b2233fd834e3b0134ae2eb4784b
Dyberg <mathias.rerup-dyberg@hotmail.com>
2023-06-08 13:46:51 +0200

    Cool stuff

--- tourguide/src/Setting.js
+++ tourguide/src/Setting.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import SettingComponent from "./SaveCustomComponent";
 
@@ -9,6 +9,7 @@ export class Setting extends React.Component {
         this.handleSettingChange = this.handleSettingChange.bind(this);
         this.handleCustomChange = this.handleCustomChange.bind(this);
         this.replaceCustom = this.replaceCustom.bind(this);
+        this.howTo = "To use this tool, first write a lambda term in the field asking you to do so. It is important to explicitly write paranthesis in the term, for example ((λx.x)(λy.y)). Additionally, an application with two variable terms needs explicit paranthesis as such: ((x)(y)) \n When a valid term has been entered, you need to select a reduction strategy in the dropdown menu. Choosing the \"Custom\" strategy, allows you to write a phased strategy yourself. It is important that you put semicolons between each phase of the strategy, for it to work properly.\n Finally, press the generate button to generate a reduction of your term using your selected strategy.";
     }
 
     handleSettingChange(e) {
@@ -28,7 +29,13 @@ export class Setting extends React.Component {
     render() {
         return (
         <div className="OptionElements">
-            <label className="label">{this.props.label}</label>
+            <div className="label-container">
+                
+                <span className="icon" value={this.howTo}>
+                    <i className="fas fa-exclamation-circle"></i>
+                </span>
+                <label className="label" id="reduce-label">{this.props.label}</label>
+            </div>
             <div className="select is-fullwidth">
                 <select onChange={this.handleSettingChange}>
                     {this.props.strats.map((strategy, index) => (
